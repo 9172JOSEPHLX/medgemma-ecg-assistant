4348544c3d0c76c0e69efb6a61b6fabdb9f1984f
JLLX <ton_email@exemple.com>
2026-02-19 16:29:52 +0100

    Expose Kaggle-ready API in package init

--- src/medgem_poc/__init__.py
+++ src/medgem_poc/__init__.py
@@ -1,2 +1,15 @@
-__all__ = ["qc", "report_schema"]
-__version__ = "0.1.0"
+# src/medgem_poc/__init__.py Feb 19th, 2026 version 16H29
+"""
+medgem_poc package (Kaggle/Jury-ready API surface)
+"""
+
+from .qc import qc_signal_from_leads
+from .fhir_export import qc_to_fhir_observation
+
+__all__ = [
+    "qc_signal_from_leads",
+    "qc_to_fhir_observation",
+]
+
+__version__ = "0.1.1"
+
